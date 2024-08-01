b9fc341292609a48f479ed6fd4e6f3d3b3eea0a7
vbyno <volodymyr.byno@gmail.com>
2024-08-01 12:05:32 +0300

    Add Ecr Registry ARN to outputs

--- ecr/outputs.tf
+++ ecr/outputs.tf
@@ -5,3 +5,7 @@ output "ecr_registry_id" {
 output "ecr_registry_url" {
   value = aws_ecr_repository.ecr_repo.repository_url
 }
+
+output "ecr_registry_arn" {
+  value = aws_ecr_repository.ecr_repo.arn
+}
