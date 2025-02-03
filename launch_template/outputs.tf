951c8db71b89547e27a2b6422213c4f8d9d0a42a
vbyno <volodymyr.byno@gmail.com>
2025-02-03 11:00:51 +0200

    Introduce RoleId output for launch template

--- launch_template/outputs.tf
+++ launch_template/outputs.tf
@@ -5,3 +5,7 @@ output "launch_template_id" {
 output "launch_template_version" {
   value = aws_launch_template.app_template.latest_version
 }
+
+output "launch_template_role_id" {
+  value = aws_iam_role.ec2_role.id
+}
